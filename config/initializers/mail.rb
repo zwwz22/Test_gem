63c355aee04ee9da38f1a6d1e4261f9eee1065f3
zwwz22 <zwwz22@sina.com>
2014-05-20 11:11:59 +0800

    mail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
     :address              => "smtp.sina.com",
     :port                 => 25,
     :domain               => 'sina.com',
-    :user_name            => '***',
-    :password             => '***',
+    :user_name            => '***111',
+    :password             => '***222',
     :authentication       => :login
     #:enable_starttls_auto => true
 }
