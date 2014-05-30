8a384d2d3d1a3d6b11e62bd92f8fd12ae6234257
zwwz22 <zwwz22@sina.com>
2014-05-30 13:06:56 +0800

    add unicorn hot restart

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,6 +6,17 @@ stdout_path "/home/zhangw/Test_gem" + "/log/unicorn.log"
 listen "/tmp/unicorn.sock"
 worker_processes 2
 timeout 30
+#hot restart
+before_fork do |server, worker|
+  old_pid = Rails.root + '/tmp/pids/unicorn.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      p  'someone else did our job for us'
+    end
+  end
+end
 #working_directory "/alidata1/xiyou/apps/toyouface/current"
 #pid "/alidata1/xiyou/apps/toyouface/current" + "/tmp/pids/unicorn.pid"
 #stderr_path "/alidata1/xiyou/apps/toyouface/current" + "/log/unicorn.log"
