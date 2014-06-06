8013ced0647530a735603bb153e45a077ae07765
zwwz22 <zwwz22@sina.com>
2014-06-06 10:09:23 +0800

    add 404 to admin

--- app/controllers/rou/application_controller.rb
+++ app/controllers/rou/application_controller.rb
@@ -19,4 +19,23 @@ class Rou::ApplicationController < ApplicationController
     login?
   end
 
+ #if Rails.env.production?
+ #  unless Rails.application.config.consider_all_requests_local
+ rescue_from Exception, with: :render_404
+ rescue_from ActionController::RoutingError, with: :render_404
+ rescue_from ActionController::UnknownController, with: :render_404
+ rescue_from ActionController::UnknownAction, with: :render_404
+ rescue_from ActiveRecord::RecordNotFound, with: :render_404
+ #end
+ #end
+
+ private
+ def render_404(exception)
+   @not_found_path = exception.message
+   respond_to do |format|
+     format.html { render template: 'rou/errors/not_found', layout: 'rou/layouts/application', status: 404 }
+     format.all { render nothing: true, status: 404 }
+   end
+ end
+
 end
